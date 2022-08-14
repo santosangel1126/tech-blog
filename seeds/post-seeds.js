3229a060324a438ca9542ee14773265c4bbe5779
Angel Santos <iiisantosjangel@gmail.com>
2022-08-14 16:10:28 -0700

    added git ignore

---
+++ seeds/post-seeds.js
@@ -0,0 +1,12 @@
+const { Post } =require('../models');
+
+const postData = [
+    {
+        title: ' Game of thrones Headless Ned ',
+        post_url: 'https://gameofthrones.fandom.com/wiki/Eddard_Stark',
+        user_id:10
+    },
+    {
+        title: ''
+    }
+]
