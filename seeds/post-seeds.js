f2360d211291a63f66b4d19f4ee77c400e0f2dea
Angel Santos <iiisantosjangel@gmail.com>
2022-08-14 16:16:14 -0700

    working on post-seeds.js

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -7,6 +7,15 @@ const postData = [
         user_id:10
     },
     {
-        title: ''
+        title: ' Naruto Sage mode ',
+        post_url: 'https://gamerant.com/naruto-sage-mode-users-ranked-strength/#:~:text=Sage%20Mode%20is%20a%20special,a%20league%20of%20their%20own.',
+        user_id: '9'
+    },
+    {
+        
     }
 ]
+
+const seedPosts = () => Post.bulkCreate(postdata);
+
+module.exports = seedPosts;
