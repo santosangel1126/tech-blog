13d03da0acd83ed3469c287b4ce2b473e285d675
Angel Santos <iiisantosjangel@gmail.com>
2022-08-14 14:25:20 -0700

    added to user-seeds.js

---
+++ seeds/user-seeds.js
@@ -0,0 +1,61 @@
+const sequelize = require('../config/connection');
+const { User, Post} = require('../models');
+
+const userdata = [
+    {
+        username: 'nedstark',
+        email: 'headless@got.com',
+        password: 'wolf01'
+    },
+    {
+        username: 'naruto07',
+        email: 'infintesorrow@hiddenleaf.com',
+        password: 'ninetail09'
+    },
+    {
+        username: 'ladiesManJiraya',
+        email: 'OgSageToad@pervysage.com',
+        password: 'narutoisthe1'
+    },
+    {
+        username: 'copyNinja',
+        email: 'Kakashi@Maskedninja.com',
+        password: 'hiddenperv'
+    },
+    {
+        username: 'FlameGod',
+        email: 'Tanjiro@Demonslayer.org',
+        password: 'Nezuko'
+    },
+    {
+        username: 'ChidoriMaster1',
+        email: 'Zenitsu@Demonslayer.org',
+        password: 'Nezuko'
+    },
+    {
+        username: 'BoarsHead',
+        email: 'StrongerthenTanjiro@Demonslayer.org',
+        password: 'DemonsSuck'
+    },
+    {
+        username: 'Monkeyboy',
+        email: 'kidGoku@Dragonball.com',
+        password: 'dragonball4'
+    },
+    {
+        username: 'Bulma_fit',
+        email: 'SpoiledGirl@findmeabf.com',
+        password: 'roshi'
+    },
+    {
+        username: 'KrillinMonk',
+        email: 'Diedtwice@loyalfriend.com',
+        password:'Marron'
+    },
+
+   
+]
+
+const seedUsers = () => User.bulkCreate(userdata, {individualHooks: true});
+
+module.exports = seedUsers;
